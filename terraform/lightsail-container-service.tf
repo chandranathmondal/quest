3a2b702ca3cccfd832ec7ce949e79edeeddb0337
Chandranath Mondal <42894367+chandranathmondal@users.noreply.github.com>
2025-02-24 02:20:10 +0530

    Update lightsail-container-service.tf

--- terraform/lightsail-container-service.tf
+++ terraform/lightsail-container-service.tf
@@ -8,3 +8,34 @@ resource "aws_lightsail_container_service" "rearc-quest" {
     Name = "rearc-quest"
   }
 }
+
+resource "aws_lightsail_container_service_deployment_version" "rearc-quest" {
+  container {
+    container_name = "rearc-quest"
+    image          = "chandranathmonda/rearc-quest:${ var.image_tag }"
+
+    command = []
+
+    environment = var.env_vars
+
+    ports = {
+      3000 = "HTTP"
+    }
+  }
+
+  public_endpoint {
+    container_name = "rearc-quest"
+    container_port = 3000
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 5
+      path                = "/"
+      success_codes       = "200-499"
+    }
+  }
+
+  service_name = aws_lightsail_container_service.rearc-quest.name
+}
