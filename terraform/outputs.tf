85105d8b6da835f894ed70a63a6faab6811ce33f
Chandranath Mondal <42894367+chandranathmondal@users.noreply.github.com>
2025-02-24 02:22:36 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "service_url" {
+output "app_runner_service_url" {
   value = aws_apprunner_service.rearc-quest.service_url
+}
+
+output "lightsail_container_service_url" {
+  value = aws_lightsail_container_service.rearc-quest.url
 }
